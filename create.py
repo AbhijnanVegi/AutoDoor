220438821bbd285d4ff9353d62d6b8dc8d283d31
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-12-02 18:02:07 +0530

    Added OM2M create

---
+++ create.py
@@ -0,0 +1,11 @@
+from onem2m import *
+
+uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
+ae = "Spoodergang"
+cnt = "MotionDetected"
+
+create_ae(uri_cse, ae)
+uri_ae = uri_cse + "/" + ae
+create_cnt(uri_ae,cnt)
+uri_cnt = uri_ae+"/" +cnt
+
