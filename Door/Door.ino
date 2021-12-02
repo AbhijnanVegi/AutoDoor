f7e520199e30cc128236d79ee6819bd8012a8ca4
Dhruv Hirpara <dhruv.hirpara@students.iiit.ac.in>
2021-12-02 22:40:38 +0530

    Update Door.ino

--- Door/Door.ino
+++ Door/Door.ino
@@ -1,24 +1,28 @@
-#include <Servo.h>
+
 #include <WiFi.h>
 #include <WebServer.h>
 #include <HTTPClient.h>
-#include "secrets.h"
+// #include "secrets.h"
 
+#include <Servo.h>
 #define trigger 33
 #define echo 32
 #define PIR 34
-
 static const int Spin = 27;
-
 Servo myservo;
-WebServer server(80);
-
-
 int thresh = 0;
 long duration;
 long distance;
 bool sense_motion = 0;
 bool com_closer = 0;
+int calibrationTime = 60;
+
+
+
+
+WebServer server(80);
+
+
 
 String serveraddr = "http://" cse_ip ":" cse_port "/~/in-cse/in-name/";
 String ae = "Spoodergang";
@@ -39,16 +43,28 @@ void createCI(String val)
 
 long getDistance(long time_taken)
 {
-  return 17 * time_taken / 1000;
+    return 17 * time_taken / 1000;
 }
 
 void setup()
 {
-  myservo.attach(Spin);
-  pinMode(echo, INPUT);
-  pinMode(trigger, OUTPUT);
-  pinMode(PIR, INPUT);
   Serial.begin(9600);
+  myservo.attach(Spin);
+    pinMode(echo, INPUT);
+    pinMode(trigger, OUTPUT);
+    pinMode(PIR, INPUT);
+     digitalWrite(PIR, LOW); 
+
+  Serial.print("calibrating sensor "); 
+  for(int i = 0; i < calibrationTime; i++)
+  { Serial.print("."); 
+    delay(1000); 
+  } 
+  Serial.println(" done"); 
+  Serial.println("SENSOR ACTIVE"); delay(50); 
+    Serial.begin(9600);
+    myservo.write(0);
+  
   // Connect to WiFi
   WiFi.begin(SSID, PASS);
 
@@ -89,83 +105,87 @@ void setup()
     delay(1000);
     myservo.write(0);
   });
-
+  
   myservo.write(0);
+  server.begin();
 }
 
 bool read_ultrasonic(int readings[])
 {
-  int readin[25];
-  bool com_closer = 1;
-  bool nomotion = 1;
-  for (int i = 0; i < 25; i++)
-  {
-    digitalWrite(trigger, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigger, LOW);
-    duration = pulseIn(echo, HIGH);
-    readin[i] = getDistance(duration);
-    delay(200);
-  }
-  int j = 0;
-  for (int i = 0; i < 25; i++)
-  {
+  int readin[15];
+    bool com_closer = 1;
+    bool nomotion = 1;
+    for (int i = 0; i < 15; i++)
+    {
+        digitalWrite(trigger, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigger, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigger, LOW);
+        duration = pulseIn(echo, HIGH);
+        readin[i] = getDistance(duration);
+        delay(100);
+    }
+    int j= 0;
+    for (int i = 0; i < 15; i++)
+    {
 
-    readings[j] += readin[i];
-    if ((i + 1) % 5 == 0)
-    {
-      readings[j] /= 5;
-      Serial.println(readings[j]);
-      j++;
+      readings[j]+= readin[i];
+      if((i+1)%5==0){
+         readings[j] /=5;
+         Serial.println(readings[j]);
+         j++;
+       }
+      
+      
+
+     
     }
-  }
-  Serial.println("-----------");
-  for (int i = 0; i < 4; i++)
-  {
-    if (readings[i + 1] - readings[i] > 3 || readings[i + 1] - readings[i] < -3)
-    {
-      nomotion = 0;
-      break;
-    }
-  }
-  if (!nomotion)
-  {
-    for (int i = 0; i < 4; i++)
-    {
-      if (readings[i + 1] - readings[i] > 10)
-      {
-        return 0;
+    Serial.println("-----------");
+   for (int i =0;i <2;i++){
+      if (readings[i+1] - readings[i] > 3 || readings[i+1] - readings[i] < -3){
+        nomotion = 0;
+        break;
       }
     }
+    if(!nomotion)
+    {
+        for (int i =0;i <2;i++){
+          if (readings[i+1] - readings[i] > 10){
+            return 0;
+          }
+        }
+  
+      return 1;
+    }
+    return 0;
 
-    return 1;
-  }
-  return 0;
 }
 
 void loop()
 {
-  sense_motion = digitalRead(PIR);
-  int readings[5] = {0};
-  com_closer = read_ultrasonic(readings);
-  if (sense_motion)
-  {
-    Serial.println("Motion detected");
-    if (com_closer)
+  server.handleClient();
+    sense_motion = digitalRead(PIR);
+    int readings[5] = {0};
+  
+    if (sense_motion)
     {
-      createCI("1");
-      myservo.write(90);
-      delay(5000);
-      myservo.write(0);
+      Serial.println("Motion detected");
+      if(read_ultrasonic(readings))
+      {
+          createCI("1");
+            myservo.write(90);
+            delay(5000);
+            myservo.write(0);
+      }
     }
-  }
-  else
-  {
-    createCI("0");
-    Serial.println("No motion detected");
-    myservo.write(0);
-  }
-  delay(200);
+    else
+    {
+      createCI("0");
+      Serial.println("No motion detected");
+        myservo.write(0);
+    }
+    
+
+    
 }
